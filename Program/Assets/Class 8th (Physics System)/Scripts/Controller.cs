4952c528e6533e1453b2530339920eb6aa29923a
jiseok08 <heoji0912@gmail.com>
2026-06-01 21:43:19 +0900

    (2026.6.1) 재질 시스템

--- Program/Assets/Class 8th (Physics System)/Scripts/Controller.cs
+++ Program/Assets/Class 8th (Physics System)/Scripts/Controller.cs
@@ -2,12 +2,18 @@ using UnityEngine;
 
 public class Controller : MonoBehaviour
 {
-    [SerializeField] float speed;
+    [SerializeField] float force;
+
     [SerializeField] Vector3 direction;
+
     [SerializeField] Rigidbody rigidbody;
+
+    [SerializeField] ForceMode forceMode;
 
     private void Start()
     {
+        forceMode = ForceMode.Force;
+
         rigidbody = GetComponent<Rigidbody>();
     }
 
@@ -37,6 +43,37 @@ public class Controller : MonoBehaviour
         // 무게(m)와 시간(t)을 모두 무시하며, 입력한 백터 값 자체가 객체의 다음
         // 프레임 속도 변화량이 되는 것입니다.
 
-        rigidbody.AddForce(direction * speed, ForceMode.Force);
+        if (forceMode == ForceMode.Impulse)
+        {
+            rigidbody.AddForce(Vector3.up * force, forceMode);
+
+            forceMode = ForceMode.Force;
+
+            return;
+        }
+
+        rigidbody.AddForce(direction * force, forceMode);
+    }
+
+    public void Soar()
+    {
+        forceMode = ForceMode.Impulse;
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Barrier")) ;
+        {
+            Debug.Log("OnCollisionEnter");
+        }
+    }
+
+    private void OnCollisionStay(Collision collision)
+    {
+        Debug.Log("OnCollisionStay");
+    }
+    private void OnCollisionExit(Collision collision)
+    {
+        Debug.Log("OnCollisionExit");
     }
 }
--- Program/Assets/Class 8th (Physics System)/Scripts/Sensor.cs
+++ Program/Assets/Class 8th (Physics System)/Scripts/Sensor.cs
@@ -7,6 +7,13 @@ public class Sensor : MonoBehaviour
     private void OnTriggerEnter(Collider other)
     {
         Debug.Log("OnTriggerEnter");
+
+        Controller controller = other.GetComponent<Controller>();
+
+        if (controller != null)
+        {
+            controller.Soar();
+        }
     }
 
     private void OnTriggerStay(Collider other)
