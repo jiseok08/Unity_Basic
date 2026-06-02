a24ca1a73ef44eb905daa7e6c7f0da2b5222a99a
jiseok08 <heoji0912@gmail.com>
2026-06-02 21:42:46 +0900

    (2026.6.2) 광선 검출 시스템

---
+++ Program/Assets/Clsaa 9th (Coroutine)/Scripts/CreateManager.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class CreateManager : MonoBehaviour
+{
+    [SerializeField] float offset = 1.5f;
+    [SerializeField] GameObject elemental;
+
+    void Start()
+    {
+        for (int i = 0; i < 5; i++)
+        {
+            GameObject clone = Instantiate(elemental, transform);
+
+            clone.transform.position = new Vector3(-3 + (offset * i), 0, 0);
+        }
+    }
+}
