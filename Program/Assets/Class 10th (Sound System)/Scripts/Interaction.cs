eb16d35af9dfe9452ac14ecae35c2b534cfbbb73
jiseok08 <heoji0912@gmail.com>
2026-06-05 21:42:59 +0900

    (2026.6.5) 카메라 시스템

--- Program/Assets/Class 10th (Sound System)/Scripts/Interaction.cs
+++ Program/Assets/Class 10th (Sound System)/Scripts/Interaction.cs
@@ -1,18 +1,32 @@
+using System.Security.Cryptography;
 using UnityEngine;
 
 public class Interaction : MonoBehaviour
 {
     [SerializeField] Ray ray;
+    [SerializeField] LayerMask layerMask;
+    [SerializeField] RaycastHit rayCastHit;
+    [SerializeField] float distance = 100.0f;
 
     private void Update()
     {
+        if (Input.GetMouseButtonDown(0))
+        {
+            ray = Camera.main.ScreenPointToRay(Input.mousePosition);
 
+            if (Physics.Raycast(ray, out rayCastHit, distance, layerMask))
+            {
+                AudioSource audioSource = rayCastHit.collider.GetComponent<AudioSource>();
+                if (audioSource.isPlaying == false)
+                {
+                    audioSource.Play();
+                }
+            }
+        }
     }
 
     private void OnDrawGizmos()
     {
-        ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-
         Gizmos.DrawRay(ray.origin, ray.direction * 100);
     }
 }
